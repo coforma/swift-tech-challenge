d0bc7ee7a03b4cd4c26413621f057f60700f3e33
Brax Excell <braxton.excell@gmail.com>
2024-03-11 18:07:16 -0500

    finalize cypress adr, tooling

--- cypress.config.js
+++
@@ -1,9 +0,0 @@
-const { defineConfig } = require("cypress");
-
-module.exports = defineConfig({
-  e2e: {
-    baseUrl: "http://localhost:3000",
-    supportFile: "src/app/testing/cypress/setupCypress.ts",
-    specPattern: "src/app/testing/cypress/**/*.cy.{js,jsx,ts,tsx}",
-  },
-});
---
+++ cypress.config.ts
@@ -0,0 +1,23 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    baseUrl: "http://localhost:3000",
+    supportFile: "src/app/testing/cypress/setupCypress.ts",
+    specPattern: "src/app/testing/cypress/**/*.cy.{js,jsx,ts,tsx}",
+    setupNodeEvents(on: any, config: any) {
+      // bind to the event we care about
+      on("task", {
+        log(message: any) {
+          console.log(message); // eslint-disable-line no-console
+          return null;
+        },
+        table(message: any) {
+          console.table(message); // eslint-disable-line no-console
+          return null;
+        },
+      });
+      return config;
+    },
+  },
+});
