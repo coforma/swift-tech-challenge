94d9c61a161027e50b9d25f1b832acfbfb970ef6
Dan Miller <dan@coforma.io>
2024-03-12 18:26:43 -0400

    WIP: Working on new ingestion, now with linting

---
+++ utilities/data-ingestion/generate-descriptions.py
@@ -0,0 +1,70 @@
+import json
+import boto3
+import random
+
+bedrock_runtime = boto3.client("bedrock-runtime", "us-east-1")
+dynamodb = boto3.resource("dynamodb")
+table = dynamodb.Table("institutions")
+s3_client = boto3.client("s3")
+
+
+def lambda_handler(event, context):
+    # load the json to a string
+    resp = json.loads(json.loads(json.dumps(event))["Records"][0]["body"])
+
+    ## variable for use throughtout function
+    institutionName = resp["institutionName"]
+    institutionId = resp["institutionId"]
+
+    # create text prompt
+    text_prompt = "Create a 45 word description for " + institutionName
+
+    # get description
+    bedrock_text_args = {
+        "modelId": "ai21.j2-mid-v1",
+        "contentType": "application/json",
+        "accept": "application/json",
+        "body": '{"prompt":"'
+        + text_prompt
+        + '","maxTokens":200,"temperature":0.8,"topP":0.9,"stopSequences":[],"countPenalty":{"scale":0},"presencePenalty":{"scale":0},"frequencyPenalty":{"scale":0}}',
+    }
+
+    generated_text_response = bedrock_runtime.invoke_model(**bedrock_text_args)
+    text_json = json.loads(generated_text_response.get("body").read())
+    resp["description"] = text_json["completions"][0]["data"]["text"]
+
+    # get image
+    image_prompts = [
+        "Can you generate me a night time full color image of",
+        "Can you generate me a full color image of",
+        "Can you generate me a winter full color image of",
+        "Can you generate me an impressionist image of",
+        "Can you generate me an post-modern fall image of",
+    ]
+
+    image_prompt = random.choice(image_prompts) + " " + institutionName
+
+    bedrock_image_args = {
+        "modelId": "amazon.titan-image-generator-v1",
+        "contentType": "application/json",
+        "accept": "application/json",
+        "body": '{"textToImageParams":{"text":"'
+        + image_prompt
+        + '"},"taskType":"TEXT_IMAGE","imageGenerationConfig":{"cfgScale":8,"seed":0,"quality":"standard","width":512,"height":512,"numberOfImages":1}}',
+    }
+
+    generated_image_response = bedrock_runtime.invoke_model(**bedrock_image_args)
+    image_body = json.loads(generated_image_response.get("body").read())
+
+    # write image to S3
+    fileName = str(institutionId) + ".json"
+    bucketName = "swift-institution-images"
+    s3_response = s3_client.put_object(
+        Body=image_body["images"][0], Bucket=bucketName, Key=fileName
+    )
+    resp["imagePath"] = bucketName + "/" + fileName
+
+    try:
+        table.put_item(Item=resp)
+    except Exception as err:
+        print(err)
--- utilities/data-ingestion/instituions.py
+++ utilities/data-ingestion/instituions.py
@@ -1,10 +1,13 @@
 import boto3
+import json
 from decimal import Decimal
+from datetime import datetime
 
 
 s3_client = boto3.client("s3")
-dynamodb = boto3.resource("dynamodb")
-table = dynamodb.Table("institutions")
+sqs = boto3.client("sqs")
+# dynamodb = boto3.resource("dynamodb")
+# table = dynamodb.Table("institutions")
 
 
 def lambda_handler(event, context):
@@ -33,172 +36,187 @@ def lambda_handler(event, context):
         institutions = institutions[:80]
         records_processed = 0
 
-        # transform data into Item and write to table
+        # transform data into Item and add to queue
         for institution in institutions:
             institution_data = institution.split(",")
-            print(institution_data[1])
             try:
-                table.put_item(
-                    Item={
-                        "institutionId": int(institution_data[0]),
-                        "recordType": "data",
-                        "description": """Immerse yourself in cutting-edge
+                sqs.send_message(
+                    QueueUrl="https://sqs.us-east-1.amazonaws.com/905418154281/process-institutions",
+                    MessageBody=json.dumps(
+                        {
+                            "institutionId": int(institution_data[0]),
+                            "recordType": "data",
+                            "updatedAt": datetime.now().strftime(
+                                "%Y-%m-%dT%H:%M:%S.%f"
+                            ),
+                            "description": """Immerse yourself in cutting-edge
                              academia at our university, where innovation meets
                              tradition. Discover a vibrant campus community
                              dedicated to fostering growth and excellence.
                              Join us in shaping tomorrow's leaders and
                              innovators!""",
-                        "institutionName": institution_data[1],
-                        "city": institution_data[3],
-                        "state": institution_data[4],
-                        "zip": institution_data[5],
-                        "latitude": convertToNumeric(institution_data[11]),
-                        "longitude": convertToNumeric(institution_data[12]),
-                        "url": institution_data[7],
-                        "control": convertToNumeric(institution_data[6], True),
-                        "predominantUndergradDegree": int(institution_data[9]),
-                        "highestDegreeAwarded": int(institution_data[10]),
-                        "specialties": {
-                            "humanities": institution_data[61],
-                            "stem": institution_data[62],
-                            "socialScience": institution_data[63],
-                            "occupational": institution_data[64],
-                            "interdisciplinary": institution_data[65],
-                        },
-                        "raceDemographics": {
-                            "percentWhite": convertToNumeric(institution_data[14]),
-                            "percentBlack": convertToNumeric(institution_data[15]),
-                            "percentAsian": convertToNumeric(institution_data[17]),
-                            "percentHispanic": convertToNumeric(institution_data[16]),
-                            "percentAian": convertToNumeric(institution_data[18]),
-                            "percentNhpi": convertToNumeric(institution_data[19]),
-                            "percentTwoOrMore": convertToNumeric(institution_data[20]),
-                            "percentNonResidentAlien": convertToNumeric(
-                                institution_data[21]
+                            "institutionName": institution_data[1],
+                            "city": institution_data[3],
+                            "state": institution_data[4],
+                            "zip": institution_data[5],
+                            "latitude": convertToNumeric(institution_data[11]),
+                            "longitude": convertToNumeric(institution_data[12]),
+                            "url": institution_data[7],
+                            "control": convertToNumeric(institution_data[6], True),
+                            "predominantUndergradDegree": int(institution_data[9]),
+                            "highestDegreeAwarded": int(institution_data[10]),
+                            "specialties": {
+                                "humanities": institution_data[61],
+                                "stem": institution_data[62],
+                                "socialScience": institution_data[63],
+                                "occupational": institution_data[64],
+                                "interdisciplinary": institution_data[65],
+                            },
+                            "raceDemographics": {
+                                "percentWhite": convertToNumeric(institution_data[14]),
+                                "percentBlack": convertToNumeric(institution_data[15]),
+                                "percentAsian": convertToNumeric(institution_data[17]),
+                                "percentHispanic": convertToNumeric(
+                                    institution_data[16]
+                                ),
+                                "percentAian": convertToNumeric(institution_data[18]),
+                                "percentNhpi": convertToNumeric(institution_data[19]),
+                                "percentTwoOrMore": convertToNumeric(
+                                    institution_data[20]
+                                ),
+                                "percentNonResidentAlien": convertToNumeric(
+                                    institution_data[21]
+                                ),
+                                "percentUnknownRace": convertToNumeric(
+                                    institution_data[22]
+                                ),
+                            },
+                            "admissionRate": convertToNumeric(institution_data[13]),
+                            "averageAttendanceCost": convertToNumeric(
+                                institution_data[35]
                             ),
-                            "percentUnknownRace": convertToNumeric(
-                                institution_data[22]
+                            "tuitionInState": convertToNumeric(institution_data[36]),
+                            "tuitionOutOfState": convertToNumeric(institution_data[37]),
+                            "percentUndergradWithLoan": convertToNumeric(
+                                institution_data[43]
                             ),
-                        },
-                        "admissionRate": convertToNumeric(institution_data[13]),
-                        "averageAttendanceCost": convertToNumeric(institution_data[35]),
-                        "tuitionInState": convertToNumeric(institution_data[36]),
-                        "tuitionOutOfState": convertToNumeric(institution_data[37]),
-                        "percentUndergradWithLoan": convertToNumeric(
-                            institution_data[43]
-                        ),
-                        "netPriceCalculatorUrl": institution_data[8],
-                        "satScores": {
-                            "satAverageScore": convertToNumeric(
-                                institution_data[60], True
+                            "netPriceCalculatorUrl": institution_data[8],
+                            "satScores": {
+                                "satAverageScore": convertToNumeric(
+                                    institution_data[60], True
+                                ),
+                                "satReadingPercentile25": convertToNumeric(
+                                    institution_data[51], True
+                                ),
+                                "satReadingPercentile50": convertToNumeric(
+                                    institution_data[57], True
+                                ),
+                                "satReadingPercentile75": convertToNumeric(
+                                    institution_data[52], True
+                                ),
+                                "satWritingPercentile25": convertToNumeric(
+                                    institution_data[55], True
+                                ),
+                                "satWritingPercentile50": convertToNumeric(
+                                    institution_data[59], True
+                                ),
+                                "satWritingPercentile75": convertToNumeric(
+                                    institution_data[56], True
+                                ),
+                                "satMathPercentile25": convertToNumeric(
+                                    institution_data[53], True
+                                ),
+                                "satMathPercentile50": convertToNumeric(
+                                    institution_data[58], True
+                                ),
+                                "satMathPercentile75": convertToNumeric(
+                                    institution_data[54], True
+                                ),
+                            },
+                            "publicNetPrice": {
+                                "averagePrice": convertToNumeric(
+                                    institution_data[23], True
+                                ),
+                                "averagePriceUnder30k": convertToNumeric(
+                                    institution_data[25], True
+                                ),
+                                "averagePriceUnder30To48k": convertToNumeric(
+                                    institution_data[26], True
+                                ),
+                                "averagePriceUnder48To75k": convertToNumeric(
+                                    institution_data[27], True
+                                ),
+                                "averagePriceUnder75To110k": convertToNumeric(
+                                    institution_data[28], True
+                                ),
+                                "averagePriceUnder110kPlus": convertToNumeric(
+                                    institution_data[29], True
+                                ),
+                            },
+                            "privateNetPrice": {
+                                "averagePrice": convertToNumeric(
+                                    institution_data[24], True
+                                ),
+                                "averagePriceUnder30k": convertToNumeric(
+                                    institution_data[30], True
+                                ),
+                                "averagePriceUnder30To48k": convertToNumeric(
+                                    institution_data[31], True
+                                ),
+                                "averagePriceUnder48To75k": convertToNumeric(
+                                    institution_data[32], True
+                                ),
+                                "averagePriceUnder75To110k": convertToNumeric(
+                                    institution_data[33], True
+                                ),
+                                "averagePriceUnder110kPlus": convertToNumeric(
+                                    institution_data[34], True
+                                ),
+                            },
+                            "facultyAverageSalary": convertToNumeric(
+                                institution_data[39]
                             ),
-                            "satReadingPercentile25": convertToNumeric(
-                                institution_data[51], True
+                            "facultyPercentageEmployedFull": convertToNumeric(
+                                institution_data[40]
                             ),
-                            "satReadingPercentile50": convertToNumeric(
-                                institution_data[57], True
+                            "studentToFacultyRatio": convertToNumeric(
+                                institution_data[47]
                             ),
-                            "satReadingPercentile75": convertToNumeric(
-                                institution_data[52], True
+                            "instructionalExpenditurePerSt": convertToNumeric(
+                                institution_data[38]
                             ),
-                            "satWritingPercentile25": convertToNumeric(
-                                institution_data[55], True
+                            "completionRates": {
+                                "fourYearInstitution": convertToNumeric(
+                                    institution_data[41]
+                                ),
+                                "underFourYearInstitution": convertToNumeric(
+                                    institution_data[42]
+                                ),
+                            },
+                            "percentCompletedInFourYears": convertToNumeric(
+                                institution_data[44]
                             ),
-                            "satWritingPercentile50": convertToNumeric(
-                                institution_data[59], True
+                            "percentCompletedInEightYears": convertToNumeric(
+                                institution_data[50]
                             ),
-                            "satWritingPercentile75": convertToNumeric(
-                                institution_data[56], True
-                            ),
-                            "satMathPercentile25": convertToNumeric(
-                                institution_data[53], True
-                            ),
-                            "satMathPercentile50": convertToNumeric(
-                                institution_data[58], True
-                            ),
-                            "satMathPercentile75": convertToNumeric(
-                                institution_data[54], True
-                            ),
-                        },
-                        "publicNetPrice": {
-                            "averagePrice": convertToNumeric(
-                                institution_data[23], True
-                            ),
-                            "averagePriceUnder30k": convertToNumeric(
-                                institution_data[25], True
-                            ),
-                            "averagePriceUnder30To48k": convertToNumeric(
-                                institution_data[26], True
-                            ),
-                            "averagePriceUnder48To75k": convertToNumeric(
-                                institution_data[27], True
-                            ),
-                            "averagePriceUnder75To110k": convertToNumeric(
-                                institution_data[28], True
-                            ),
-                            "averagePriceUnder110kPlus": convertToNumeric(
-                                institution_data[29], True
-                            ),
-                        },
-                        "privateNetPrice": {
-                            "averagePrice": convertToNumeric(
-                                institution_data[24], True
-                            ),
-                            "averagePriceUnder30k": convertToNumeric(
-                                institution_data[30], True
-                            ),
-                            "averagePriceUnder30To48k": convertToNumeric(
-                                institution_data[31], True
-                            ),
-                            "averagePriceUnder48To75k": convertToNumeric(
-                                institution_data[32], True
-                            ),
-                            "averagePriceUnder75To110k": convertToNumeric(
-                                institution_data[33], True
-                            ),
-                            "averagePriceUnder110kPlus": convertToNumeric(
-                                institution_data[34], True
-                            ),
-                        },
-                        "facultyAverageSalary": convertToNumeric(institution_data[39]),
-                        "facultyPercentageEmployedFull": convertToNumeric(
-                            institution_data[40]
-                        ),
-                        "studentToFacultyRatio": convertToNumeric(institution_data[47]),
-                        "instructionalExpenditurePerSt": convertToNumeric(
-                            institution_data[38]
-                        ),
-                        "completionRates": {
-                            "fourYearInstitution": convertToNumeric(
-                                institution_data[41]
-                            ),
-                            "underFourYearInstitution": convertToNumeric(
-                                institution_data[42]
-                            ),
-                        },
-                        "percentCompletedInFourYears": convertToNumeric(
-                            institution_data[44]
-                        ),
-                        "percentCompletedInEightYears": convertToNumeric(
-                            institution_data[50]
-                        ),
-                        "earnings": {
-                            "meanGraduateEarnings10Years": convertToNumeric(
-                                institution_data[45], True
-                            ),
-                            "medianGraduateEarnings10Years": convertToNumeric(
-                                institution_data[46], True
-                            ),
-                        },
-                        "retentionRate": {
-                            "fourYearInstitution": convertToNumeric(
-                                institution_data[48]
-                            ),
-                            "underFourYearInstitution": convertToNumeric(
-                                institution_data[49]
-                            ),
-                        },
-                    },
+                            "earnings": {
+                                "meanGraduateEarnings10Years": convertToNumeric(
+                                    institution_data[45], True
+                                ),
+                                "medianGraduateEarnings10Years": convertToNumeric(
+                                    institution_data[46], True
+                                ),
+                            },
+                            "retentionRate": {
+                                "fourYearInstitution": convertToNumeric(
+                                    institution_data[48]
+                                ),
+                                "underFourYearInstitution": convertToNumeric(
+                                    institution_data[49]
+                                ),
+                            },
+                        }
+                    ),
                 )
 
             except Exception as e:
