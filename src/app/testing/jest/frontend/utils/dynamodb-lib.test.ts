9d9cae0c4b6a955883fb47be8d0c75b9b097d9c7
Garrett Rabian <57802560+gmrabian@users.noreply.github.com>
2024-03-13 09:30:56 -0400

    add scan function to dynamodb lib (#55)

--- src/app/testing/jest/frontend/utils/dynamodb-lib.test.ts
+++ src/app/testing/jest/frontend/utils/dynamodb-lib.test.ts
@@ -1,5 +1,9 @@
 import dynamoLib from "../../../../utils/dynamodb-lib";
-import { GetCommand, DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
+import {
+  GetCommand,
+  DynamoDBDocumentClient,
+  ScanCommand,
+} from "@aws-sdk/lib-dynamodb";
 import { mockClient } from "aws-sdk-client-mock";
 
 const dynamoClientMock = mockClient(DynamoDBDocumentClient);
@@ -15,5 +19,14 @@ describe("Test DynamoDB lib", () => {
     await dynamoLib.get({ TableName: "foos", Key: { id: "foo1" } });
 
     expect(mockGet).toHaveBeenCalled();
+  });
+
+  test("Can perform a single scan", async () => {
+    const mockItem = { foo: "bar" };
+    dynamoClientMock.on(ScanCommand).resolves({ Items: [mockItem] });
+
+    const result = await dynamoLib.singleScan({ TableName: "foos" });
+
+    expect(result.Items?.[0]).toBe(mockItem);
   });
 });
