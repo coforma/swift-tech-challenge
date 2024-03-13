cd54c9f4ed16a86c7231a2585bdeea174f6a270d
Nick Summers <ntsummers1@proton.me>
2024-03-13 19:00:34 -0400

    Bring back accessibility tests for CardIcon and CollegeCard (#68)
    Co-authored-by: Brax Excell <braxton.excell@gmail.com>

--- src/app/testing/jest/frontend/components/cards/CollegeCard.test.tsx
+++ src/app/testing/jest/frontend/components/cards/CollegeCard.test.tsx
@@ -1,6 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import { College, CollegeType } from "@/src/app/types";
 import { CollegeCard } from "@/src/app/components";
+import { axe } from "jest-axe";
 
 const college: College = {
   id: 0,
@@ -15,7 +16,11 @@ const college: College = {
   avgCost: 17980,
 };
 
-const component = <CollegeCard college={college} />;
+const component = (
+  <ul>
+    <CollegeCard college={college} />
+  </ul>
+);
 
 describe("Test CollegeCard", () => {
   beforeEach(() => {
@@ -30,5 +35,13 @@ describe("Test CollegeCard", () => {
     expect(screen.getByText(college.populationAmount)).toBeVisible();
     expect(screen.getByText(college.gradRate)).toBeVisible();
     expect(screen.getByText(college.avgCost)).toBeVisible();
+  });
+});
+
+describe("Test CollegeCard accessibility", () => {
+  it("Should not have basic accessibility issues", async () => {
+    const { container } = render(component);
+    const results = await axe(container);
+    expect(results).toHaveNoViolations();
   });
 });
