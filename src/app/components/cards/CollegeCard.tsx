26dfcc53867e618dce27069c544147e0c817c637
Nicholas Summers <ntsummers1@proton.me>
2024-03-14 13:08:17 -0400

    Tweak typography and spacing for browse page

--- src/app/components/cards/CollegeCard.tsx
+++ src/app/components/cards/CollegeCard.tsx
@@ -15,11 +15,11 @@ import { College } from "../../types";
 export const CollegeCard = ({ college }: Props) => {
   return (
     <Card layout="flagDefault" headerFirst={true} className="card">
-      <CardHeader>
+      <CardHeader className="card_header">
         <h2 className="card_header-title">{college.name}</h2>
         <p className="card_header-subtitle">{`${college.city}, ${college.state}`}</p>
       </CardHeader>
-      <CardMedia>{college.img}</CardMedia>
+      <CardMedia className="card_media">{college.img}</CardMedia>
       <CardBody>
         <p className="card_desc">{college.description}</p>
         <div className="card_grid">
@@ -35,10 +35,13 @@ export const CollegeCard = ({ college }: Props) => {
           />
         </div>
       </CardBody>
-      <CardFooter>
+      <CardFooter className="card_footer">
         <ButtonGroup>
           <Button name="apply" type={"button"}>
             Apply to this school
+          </Button>
+          <Button name="information" type={"button"} outline={true}>
+            More information
           </Button>
         </ButtonGroup>
       </CardFooter>
