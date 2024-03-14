42e9fcd72566a47f1a27d4d9a8c5860670311553
Eric Cuevas <145045939+cuevas1876@users.noreply.github.com>
2024-03-14 12:40:11 -0500

    Issue (#91) - Images not displaying - Set cloudformation origin request to "Managed-AllViewerExceptHostHeader"

--- tf/modules/frontend/cloudfront.tf
+++ tf/modules/frontend/cloudfront.tf
@@ -2,6 +2,10 @@ data "aws_cloudfront_cache_policy" "disabled" {
   name = "Managed-CachingDisabled"
 }
 
+data "aws_cloudfront_origin_request_policy" "all" {
+  name = Managed-AllViewerExceptHostHeader
+}
+
 
 resource "aws_cloudfront_distribution" "distribution" {
   origin {
@@ -31,13 +35,14 @@ resource "aws_cloudfront_distribution" "distribution" {
     allowed_methods = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
     cached_methods  = ["GET", "HEAD"]
 
-    viewer_protocol_policy = "allow-all"
-    min_ttl                = 0
-    cache_policy_id        = data.aws_cloudfront_cache_policy.disabled.id
-    compress               = true
-    default_ttl            = 0
-    max_ttl                = 0
-    target_origin_id       = local.api_gw_origin_id
+    viewer_protocol_policy   = "allow-all"
+    min_ttl                  = 0
+    cache_policy_id          = data.aws_cloudfront_cache_policy.disabled.id
+    origin_request_policy_id = data.aws_cloudfront_origin_request_policy.all.id
+    compress                 = true
+    default_ttl              = 0
+    max_ttl                  = 0
+    target_origin_id         = local.api_gw_origin_id
   }
   restrictions {
     geo_restriction {
