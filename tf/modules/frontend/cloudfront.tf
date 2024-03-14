201e62691b1d2c2fabb8da940a509f0d8ed4938f
Eric Cuevas <145045939+cuevas1876@users.noreply.github.com>
2024-03-14 09:21:54 -0500

    Issue/front end infra (#73)
    * Issue front end infra (#5)
    * App Deploy
    
    * Issue front end infra - fix deploy syntax
    
    * Issue front end infra (#5)
    * Application Front end Terraform
    
    * Issue front end (#5)
    * Application Testing pipeline
    
    * Issue front end infra (#5) - pipeline trigger fix
    
    * Issue front end infra (#5) - pipeline typo
    
    * Issue front end infra (#5) - pipeline syntax fix
    
    * Issue front end infra (#5) - pipeline typo
    
    * Issue front end infra (#5) - pipeline typo
    
    * Issue front end infra (#5) - testing pipeline conditionals
    
    * Issue front end infra (#5) - pipeline typo
    
    * Issue front end infra - fix pipeline script
    
    * Issue front end infra - fix pipeline script
    
    * Issue front end infra - fix pipeline script
    
    * Issue front end infra - fix pipeline script
    
    * Issue front end infra - fix pipeline script
    
    * Issue front end infra - fix integration tests
    
    * Issue front end infra - fix pipeline
    
    * Issue front end infra - fix pipeline
    
    * Issue front end infra - fix pipeline
    
    * Issue front end infra - fix pipeline
    
    * Issue front end infra - fix pipeline
    
    * Issue front end infra - fix pipeline
    
    * Issue front end infra - fix pipeline
    
    * Issue front end infra - change cypress test to relative path
    
    * Issue front end - pipeline cleanup
    
    * Issue front end - pipeline cleanup
    
    * Issue front end - removed commented code
    
    * Issue front end - remove test tf project from pipeline
    
    * Issue front end - Update CICD pipeline filters
    
    * Issue front end - Fix pipeline concurrency deadlock
    
    * Issue front end - Fix TF plan condition
    
    * Issue front end - Add descriptions to terraform variables
    
    * Issue front end - Terraform formatting

---
+++ tf/modules/frontend/cloudfront.tf
@@ -0,0 +1,53 @@
+data "aws_cloudfront_cache_policy" "disabled" {
+  name = "Managed-CachingDisabled"
+}
+
+
+resource "aws_cloudfront_distribution" "distribution" {
+  origin {
+    domain_name         = local.api_gw_origin_id
+    origin_id           = local.api_gw_origin_id
+    connection_attempts = 3
+    connection_timeout  = 10
+
+    custom_origin_config {
+      http_port                = 80
+      https_port               = 443
+      origin_keepalive_timeout = 5
+      origin_protocol_policy   = "https-only"
+      origin_read_timeout      = 30
+      origin_ssl_protocols = [
+        "TLSv1.2",
+      ]
+    }
+  }
+
+  enabled         = true
+  is_ipv6_enabled = true
+
+  #TODO: Add logging
+
+  default_cache_behavior {
+    allowed_methods = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods  = ["GET", "HEAD"]
+
+    viewer_protocol_policy = "allow-all"
+    min_ttl                = 0
+    cache_policy_id        = data.aws_cloudfront_cache_policy.disabled.id
+    compress               = true
+    default_ttl            = 0
+    max_ttl                = 0
+    target_origin_id       = local.api_gw_origin_id
+  }
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+      locations        = []
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+  price_class = "PriceClass_100"
+}
---
+++ tf/projects/application/variables.tf
@@ -0,0 +1,37 @@
+variable "artifact_bucket" {
+  type        = string
+  description = "Bucket containing Lambda deployable artifacts"
+}
+
+variable "environment" {
+  type        = string
+  description = "Environment label for resources"
+}
+
+variable "frontend_artifact_path" {
+  type        = string
+  description = "Path within S3 bucket where var.frontend_zip_file_name can be found"
+}
+
+variable "frontend_zip_filename" {
+  type        = string
+  default     = "frontend.zip"
+  description = "Filename of zip file containing frontend lambda deployable artifact"
+}
+
+variable "github_repo" {
+  type        = string
+  default     = "coforma/swift-tech-challenge"
+  description = "Github repo to grant lambda deployment permissions to"
+}
+
+variable "app_deploy_restrictions" {
+  type        = list(string)
+  default     = ["environment:prod"]
+  description = "Github branches or environments to grant lambda deployment permissions to"
+}
+
+variable "bootstrap_remote_state_config" {
+  type        = map(string)
+  description = "Values for terraform_remote_state configuration for bootstrap s3 backend"
+}
