a48af4085a280479957ab16a28546c644483b3b6
Eric Cuevas <145045939+cuevas1876@users.noreply.github.com>
2024-03-14 23:49:56 -0500

    Remove unused

--- next.config.mjs
+++ next.config.mjs
@@ -3,16 +3,6 @@ const nextConfig = {
     includePaths: ["./node_modules/@uswds/uswds/packages"],
   },
   output: 'standalone',
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'public-static-3a96b108.s3.amazonaws.com',
-        port: '',
-        pathname: '/**',
-      },
-    ],
-  },
    assetPrefix: process.env.CDN_HOST ? 'https://d2z6xcoh14f3wb.cloudfront.net' : ''
 };
 
