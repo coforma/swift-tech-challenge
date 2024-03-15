07943bb85820c8ee72a772a07e6a6431323433bd
Brax Excell <braxton.excell@gmail.com>
2024-03-15 12:06:58 -0500

    Create and consume context api provider (#113)
    * add provider
    
    * fixes
    
    * finalize consumption
    
    * add tests
    
    * fix ts errors
    
    * fix one type

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 import type { Metadata } from "next";
 //  components
 import { InitAnalytics } from "./utils/analytics";
-import { PageHeader, PageFooter } from "./components";
+import { PageHeader, PageFooter, InstitutionProvider } from "./components";
 // styles
 import "@trussworks/react-uswds/lib/uswds.css";
 import "@trussworks/react-uswds/lib/index.css";
@@ -31,7 +31,7 @@ export default function RootLayout({
       </head>
       <body className={inter.className}>
         <PageHeader />
-        {children}
+        <InstitutionProvider>{children}</InstitutionProvider>
         <PageFooter />
       </body>
     </html>
