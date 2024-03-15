bf5b3b883ac5d279cd0901ca4478716c45cf43e3
Eric Cuevas <145045939+cuevas1876@users.noreply.github.com>
2024-03-15 07:00:28 -0500

    Fix assetPrefix thing

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ const nextConfig = {
     includePaths: ["./node_modules/@uswds/uswds/packages"],
   },
   output: 'standalone',
-   assetPrefix: process.env.CDN_HOST ? 'https://d2z6xcoh14f3wb.cloudfront.net' : ''
+   assetPrefix: process.env.CDN_HOST ? process.env.CDN_HOST : ''
 };
 
 export default nextConfig;
