653a820d5dd87b15c3e3924079937f78ea3772df
Eric Cuevas <145045939+cuevas1876@users.noreply.github.com>
2024-03-15 14:30:20 -0500

    Issue/95 perf changes (#118)
    * Issue (#95) Performance Improvements
    * Add CDN for static assets
    * Add provisioned resources for lambda
    * Update permissions for deployment
    
    * Issue (#95) Performance Improvements
    * Set assetPrefix from env
    
    * Issue (#95) Performance Improvements
    * Add github workflow steps for bundling and deploying static assets
    
    * Fix TF vars for stage and prod
    
    ---------
    
    Co-authored-by: Dan Miller <TheDanMiller@users.noreply.github.com>

--- next.config.mjs
+++ next.config.mjs
@@ -13,6 +13,7 @@ const nextConfig = {
     includePaths: ["./node_modules/@uswds/uswds/packages"],
   },
   output: 'standalone',
+   assetPrefix: process.env.CDN_HOST ? process.env.CDN_HOST : ''
 };
 
 export default nextConfig;
