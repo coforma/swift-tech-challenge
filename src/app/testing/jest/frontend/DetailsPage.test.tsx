3bd4243e6a0aa1f77e46c8b6af5fc6d8d8285099
Garrett Rabian <garrett.rabian@coforma.io>
2024-03-18 12:03:12 -0400

    add details page tests

---
+++ src/app/testing/jest/frontend/DetailsPage.test.tsx
@@ -0,0 +1,38 @@
+import { render, screen } from "@testing-library/react";
+import { axe } from "jest-axe";
+import InstitutionDetails from "@/src/app/[id]/page";
+import { InstitutionContext } from "@/src/app/components";
+import { InstitutionContextShape } from "@/src/app/components/institutions/InstitutionProvider";
+import { mockCollege } from "../setupJest";
+
+const testParams = {
+  id: 0,
+};
+
+const testContext: InstitutionContextShape = { institutionData: [mockCollege] };
+
+const testDetailsPageComponent = () => (
+  <InstitutionContext.Provider value={testContext}>
+    <InstitutionDetails params={testParams} />
+  </InstitutionContext.Provider>
+);
+
+describe("Test InstitutionDetails Page", () => {
+  test("Check that page renders", () => {
+    render(testDetailsPageComponent());
+    expect(screen.getByText("Name of school")).toBeVisible();
+    expect(screen.getByText("Outcomes")).toBeVisible();
+    expect(screen.getByText("Admissions")).toBeVisible();
+    expect(screen.getByText("Student body")).toBeVisible();
+    expect(screen.getByText("Academics")).toBeVisible();
+    expect(screen.getByText("Costs")).toBeVisible();
+  });
+});
+
+describe("Test InstitutionDetails Page accessibility", () => {
+  it("Should not have basic accessibility issues", async () => {
+    const { container } = render(testDetailsPageComponent());
+    const results = await axe(container);
+    expect(results).toHaveNoViolations();
+  });
+});
