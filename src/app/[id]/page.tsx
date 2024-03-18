b6745ad49e7c04ad4565ca1ab14a73288977b495
Garrett Rabian <garrett.rabian@coforma.io>
2024-03-18 15:54:37 -0400

    fix navigation to show not found if id does not exist

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -6,6 +6,7 @@ import {
   DetailsCards,
   InstitutionContext,
 } from "@/src/app/components";
+import NotFound from "@/src/app/not-found";
 // types
 import { College } from "../types";
 
@@ -19,22 +20,25 @@ export default function InstitutionDetails({
   params: { id: number };
 }) {
   const { institutionData } = useContext(InstitutionContext);
+  const [loading, setLoading] = useState(true);
   const [selectedCollege, setSelectedCollege] = useState<College>();
   useEffect(() => {
     if (institutionData) {
       setSelectedCollege(filterCollege(institutionData!, params.id));
+      setLoading(false);
     }
   }, [institutionData, params.id]);
-  return (
-    <main>
-      {!selectedCollege ? (
-        <div>please wait...</div>
-      ) : (
-        <ul className="usa-card-group">
-          <BannerCard key={params.id} college={selectedCollege} />
-          <DetailsCards key={`${params.id}-dets`} college={selectedCollege} />
-        </ul>
-      )}
-    </main>
-  );
+
+  const view = () => {
+    return !selectedCollege ? (
+      <NotFound />
+    ) : (
+      <ul className="usa-card-group">
+        <BannerCard key={params.id} college={selectedCollege} />
+        <DetailsCards key={`${params.id}-dets`} college={selectedCollege} />
+      </ul>
+    );
+  };
+
+  return <main>{loading ? <div>please wait...</div> : view()}</main>;
 }
