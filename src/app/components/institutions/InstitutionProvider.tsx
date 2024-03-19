e33b830223f1957c59c3a4b497b3a04dcf7b8b8b
Brax Excell <braxton.excell@gmail.com>
2024-03-18 19:11:51 -0500

    save data in both formats; both exposed via provider

--- src/app/components/institutions/InstitutionProvider.tsx
+++ src/app/components/institutions/InstitutionProvider.tsx
@@ -6,22 +6,32 @@ import { College } from "../../types";
 
 export interface InstitutionContextShape {
   institutionsArray: College[] | undefined;
+  institutionsObject: { [key: string]: College } | undefined;
 }
 
 export const InstitutionContext = createContext<InstitutionContextShape>({
   institutionsArray: undefined as College[] | undefined,
+  institutionsObject: undefined as { [key: string]: College } | undefined,
 });
 
 export const InstitutionProvider = ({ children }: Props) => {
   const [institutionsArray, setInstitutionsArray] = useState<
     College[] | undefined
   >();
+  const [institutionsObject, setInstitutionsObject] = useState<
+    { [key: string]: College } | undefined
+  >();
 
   const fetchInstitutions = async () => {
     try {
       const result = await getInstitutions();
       setInstitutionsArray(result);
-      // TODO: setInstitutionsObj();
+      // create object from array, indexed by institution id
+      const objectForm: { [key: string]: College } = result.reduce(
+        (acc, val) => ({ ...acc, [val.id]: val }),
+        {},
+      );
+      setInstitutionsObject(objectForm);
     } catch (e: any) {
       throw new Error("Institution data could not be loaded.");
     }
@@ -34,8 +44,9 @@ export const InstitutionProvider = ({ children }: Props) => {
   const providerValue = useMemo(
     () => ({
       institutionsArray,
+      institutionsObject,
     }),
-    [institutionsArray],
+    [institutionsArray, institutionsObject],
   );
 
   return (
