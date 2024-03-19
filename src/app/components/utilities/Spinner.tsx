de28af44c7457668ca3a5a360ea185278873ac91
Nicholas Summers <ntsummers1@proton.me>
2024-03-19 08:56:49 -0400

    Center spinner on page

--- src/app/components/utilities/Spinner.tsx
+++ src/app/components/utilities/Spinner.tsx
@@ -1,10 +1,12 @@
 export const Spinner = () => {
   return (
-    <div
-      title="spinner"
-      className="spinner"
-      role="progressbar"
-      aria-valuetext="loading"
-    ></div>
+    <div className="spinner-container">
+      <div
+        title="spinner"
+        className="spinner"
+        role="progressbar"
+        aria-valuetext="loading"
+      ></div>
+    </div>
   );
 };
