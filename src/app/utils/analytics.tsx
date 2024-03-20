5db932c33d8813680e8a87bd2efe83a622800661
Brax Excell <braxton.excell@gmail.com>
2024-03-19 19:15:39 -0500

    fix mixpanel to use local vars

--- src/app/utils/analytics.tsx
+++ src/app/utils/analytics.tsx
@@ -5,7 +5,13 @@ import mixpanel from "mixpanel-browser";
 export const InitAnalytics = () => {
   useEffect(() => {
     const isProd = process.env.NODE_ENV === "production";
-    mixpanel.init(process.env.NEXT_PUBLIC_MIXPANEL_PROJECT_TOKEN || "", {
+    const tokenMap: any = {
+      prod: "68858e4ca428836ed94d18a0faa8e338", // pragma: allowlist-secret
+      lower: "9e4e035518e9c13c5ebf624717a1d5bc", // pragma: allowlist-secret
+    };
+    // note: this variable is not a secret: https://developer.mixpanel.com/reference/project-token
+    const mixpanelToken = isProd ? tokenMap.prod : tokenMap.lower;
+    mixpanel.init(mixpanelToken, {
       debug: true,
       ignore_dnt: !isProd,
       track_pageview: "full-url",
